ec3a07216dfa1a868b356a81de1163e5cd3f23a4
harikrishnaprasad <chenagani_harikrishna@yahoo.com>
2013-05-14 23:41:03 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smart::Application.config.secret_token = 'd5320ed6c04dbdb1040c4a2332f6a22abc2ee58a13db1a2f15d8477470e322156c888e99da1d2baa1e434e021d82b1e56bdd925ed768cf4057f64885a98b7525'
---
+++ db/migrate/20130514163422_create_users.rb~
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+	t.string 	:name
+	t.text		:body
+	t.string	:number
+	t.string	:mail_id
+
+      t.timestamps
+    end
+  end
+end
